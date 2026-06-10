1523294cb72b3009c3b471284540374701cfd161
vjawarenessmedia-cloud <vjawarenessmedia@gmail.com>
2026-06-10 11:00:14 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-$host = 'localhost';
+$host = 'mysql-tth3.railway.internal';
 $dbname = 'fleet_management';
 $username = 'root';
-$password = '';
+$password = 'CtiAuRnrgONliOmoZtCJgMySnaFNZorC';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
